e89113bc366846af4d3d2f5ef49e5995518dab3b
christian thompson <40225101+cjthomp2005@users.noreply.github.com>
2019-02-16 17:03:01 -0600

    updated ping function
    now it says random shit, your welcome :)

--- Bot.js
+++ Bot.js
@@ -6,6 +6,16 @@ const Discord = require('discord.js');
 const config = require ("./config.json");
 const client = new Discord.Client();
 const prefix = ("suzu:");
+const pinglist = [
+  "heya! ",
+  "heya! ",
+  "whats up? ",
+  "sorry if im late! ",
+  "did someone say something? ",
+  "tell AstralMod I said hi! ",
+  "to ping or not to ping, ",
+  "heya! "
+];
 const activities_list = [
   "with the suzu:help command.", 
   "Hovercar Dodge!",
@@ -29,7 +39,23 @@ const activities_list = [
   "Astroneer",
   "Pirates in the car I be in",
   "WhoCanGetTheFluFirst",
-  "you thought i would put something funny here didnt you"
+  "you thought i would put something funny here didnt you",
+  "Subnautica",
+  "Minecraft",
+  "Mii channel music",
+  "Wii",
+  "Asphalt 9 Legends",
+  "Monument Valley",
+  "Rocket League",
+  "Geometry Dash",
+  "Terra Tech",
+  "Cuphead",
+  "Cities: Skylines",
+  "Scrap Mechanic",
+  "with google",
+  "Hulu",
+  "pinging @Swingin30",
+  "the crash report screen"
   ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on('ready', () => {
@@ -39,6 +65,9 @@ client.on('ready', () => {
     const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
     client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
   }, 10000); // Runs this every 10 seconds.
+  client.guilds.forEach((guild) => {
+    console.log(" - " + guild.name)
+  });
 });
 
 
@@ -46,10 +75,13 @@ client.on('message', msg => {
   if (!msg.guild) return;
   //And now, commands
   if (msg.content === prefix + 'ping') {
-    msg.reply('im not dead! it took ' + client.ping + 'ms to respond.');
+    console.log(client.ping);
+    const pmessage = Math.floor(Math.random() * (pinglist.length - 1) + 1);
+    var round =Math.round(client.ping); 
+    msg.reply((pinglist[pmessage]) + 'it took ' + round + 'ms to respond.');
   }
   else if (msg.content === prefix + 'help'){
-    msg.channel.send ('Hi there! below you can see some of my commands\n\nFirst off, you should know that my prefix is `suzu:` and will always be `suzu:`, use this in front of each command.\n\nping\npic (mention someone here)\n\nim always being updated because ree has the flu and has litteraly nothing else to do so if you have a suggestion, ping him!')
+    msg.channel.send ('Hi there! below you can see some of my commands\n\nFirst off, you should know that my prefix is `suzu:` and will always be `suzu:`, use this in front of each command.\n\nping\npic (mention someone here)\n\nim always being updated and my owner has litteraly nothing else to do so if you have a suggestion, ping him!\n\nplease note that my code is mostly ServerLion\'s because Ree cant code')
   }
   else if (msg.content.startsWith (prefix + 'pic')){
     let user = msg.mentions.users.first();
@@ -103,6 +135,50 @@ client.on('message', msg => {
     } else {
       msg.reply('You didn\'t mention the user to kick!');
     }
+  }
+  else if (msg.content.startsWith(prefix + 'ban')) {
+    if (msg.author.id != "472923135965003786"){
+      msg.reply("Hold up! You aren't Ree!");
+      return;
+    }
+    let user = msg.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = msg.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Ban the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         * Read more about what ban options there are over at
+         * https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=ban
+         */
+        member.ban({
+          reason: 'They were bad!',
+        }).then(() => {
+          // We let the message author know we were able to ban the person
+          msg.reply(`Successfully banned ${user.tag}`);
+        }).catch(err => {
+          // An error happened
+          // This is generally due to the bot not being able to ban the member,
+          // either due to missing permissions or role hierarchy
+          msg.reply('I was unable to ban the member');
+          // Log the error
+          console.error(err);
+        });
+      } else {
+        // The mentioned user isn't in this guild
+        msg.reply('That user isn\'t in this guild!');
+      }
+    } else {
+    // Otherwise, if no user was mentioned
+      msg.reply('You didn\'t mention the user to ban!');
+    }
+  }
+  else if(msg.content.startsWith(prefix)){
+    msg.reply('this is an unknown command, you can use `suzu:help` for more information on what my commands are.')
   }
 });
 
